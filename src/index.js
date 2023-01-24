7a6194405be4cb0342dde181a13507b535e53013
paulgilchrist <PGilchrist@pulte.com>
2023-01-24 15:48:24 -0500

    Simplify and organize folders

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-import { utilities } from './utilities';
+import { utilities } from './libraries/utilities';
 
 // Get initialState
 let initialPortfolio = null;
@@ -47,7 +46,3 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
